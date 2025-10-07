ab5b42ae4cc885a822531110926790422e747701
Risto Salama <risto.salama@gofore.com>
2025-10-07 11:05:26 +0300

    Add accessibility test

--- oma-opiskelijavalinta-ui/playwright/etusivu.spec.ts
+++ oma-opiskelijavalinta-ui/playwright/etusivu.spec.ts
@@ -1,12 +1,18 @@
 import { expect, test } from '@playwright/test';
+import {expectPageAccessibilityOk} from "./lib/playwrightUtils";
 
-test.describe('Etusivu', () => {
+test('Näyttää etusivun', async ({ page }) => {
+    await page.goto('');
+    await expect(page).toHaveTitle('Oma Opiskelijavalinta');
+    await expect(
+        page.getByText('Tervetuloa Oma-opiskelijavalintaan!'),
+    ).toBeVisible();
+});
 
-    test('Näyttää etusivun', async ({ page }) => {
-        await page.goto('');
-        await expect(page).toHaveTitle('Oma Opiskelijavalinta');
-        await expect(
-            page.getByText('Tervetuloa Oma-opiskelijavalintaan!'),
-        ).toBeVisible();
-    });
+test('Etusivun saavutettavuus', async ({ page }) => {
+    await page.goto('');
+    await expect(
+        page.getByText('Tervetuloa Oma-opiskelijavalintaan!'),
+    ).toBeVisible();
+    await expectPageAccessibilityOk(page);
 });
