a56a1581d3abc7ca752bb227601c30afafecd58a
Risto Salama <risto.salama@gofore.com>
2025-10-07 12:40:30 +0300

    Add license and configure husky and eslint

--- oma-opiskelijavalinta-ui/eslint.config.js
+++ oma-opiskelijavalinta-ui/eslint.config.js
@@ -5,67 +5,68 @@ import eslintReact from '@eslint-react/eslint-plugin';
 import playwright from 'eslint-plugin-playwright';
 
 const config = ts.config(
-    js.configs.recommended,
-    ts.configs.strict,
-    ts.configs.stylistic,
-    eslintConfigPrettier,
-    {
-        ignores: [
-            'node_modules',
-            '.next',
-            'out',
-            '.react-router',
-            'build',
-            '.lintstagedrc.js',
-            'coverage',
-        ],
+  js.configs.recommended,
+  ts.configs.strict,
+  ts.configs.stylistic,
+  eslintConfigPrettier,
+  {
+    ignores: [
+      'node_modules',
+      '.next',
+      'out',
+      '.react-router',
+      'build',
+      '.lintstagedrc.js',
+      'coverage',
+      'eslint.config.js',
+    ],
+  },
+  {
+    languageOptions: {
+      parser: ts.parser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
-    {
-        languageOptions: {
-            parser: ts.parser,
-            parserOptions: {
-                projectService: true,
-                tsconfigRootDir: import.meta.dirname,
+    rules: {
+      '@typescript-eslint/consistent-type-definitions': 'off',
+      '@typescript-eslint/no-inferrable-types': 'off',
+      'no-shadow': 'error',
+      'no-restricted-imports': [
+        'error',
+        {
+          patterns: ['@mui/*/*/*', '../../*'],
+          paths: [
+            {
+              name: '@mui/material',
+              importNames: ['styled'],
+              message:
+                'Please use styled from @/lib/theme or @mui/material/styles instead.',
             },
+          ],
         },
-        rules: {
-            '@typescript-eslint/consistent-type-definitions': 'off',
-            '@typescript-eslint/no-inferrable-types': 'off',
-            'no-shadow': 'error',
-            'no-restricted-imports': [
-                'error',
-                {
-                    patterns: ['@mui/*/*/*', '../../*'],
-                    paths: [
-                        {
-                            name: '@mui/material',
-                            importNames: ['styled'],
-                            message:
-                                'Please use styled from @/lib/theme or @mui/material/styles instead.',
-                        },
-                    ],
-                },
-            ],
-            '@typescript-eslint/array-type': [
-                'error',
-                {
-                    default: 'generic',
-                },
-            ],
+      ],
+      '@typescript-eslint/array-type': [
+        'error',
+        {
+          default: 'generic',
         },
+      ],
     },
-    {
-        extends: [eslintReact.configs['recommended-typescript']],
-        files: ['src/**/*.{ts,tsx}'],
+  },
+  {
+    extends: [eslintReact.configs['recommended-typescript']],
+    files: ['src/**/*.{ts,tsx}'],
+  },
+  {
+    extends: [playwright.configs['flat/recommended']],
+    files: ['playwright/**/*.ts'],
+    rules: {
+      '@typescript-eslint/no-floating-promises': 'error',
+      'playwright/expect-expect': 'off',
     },
-    {
-        extends: [playwright.configs['flat/recommended']],
-        files: ['playwright/**/*.ts'],
-        rules: {
-            '@typescript-eslint/no-floating-promises': 'error',
-            'playwright/expect-expect': 'off',
-        },
-    },
+  },
 );
 
 export default config;
