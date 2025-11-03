f4ce3e03f08bd99688bfd4e479caea51d344ae7d
Risto Salama <risto.salama@gofore.com>
2025-11-03 13:38:25 +0200

    Näytä hakemuksen runko

--- oma-opiskelijavalinta-ui/src/components/Applications.tsx
+++ oma-opiskelijavalinta-ui/src/components/Applications.tsx
@@ -1,12 +1,54 @@
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { OphTypography } from '@opetushallitus/oph-design-system';
 import { PaperWithTopColor } from './PaperWithTopColor';
 import { styled } from '@/lib/theme';
 import { useTranslations } from '@/hooks/useTranslations';
+import { QuerySuspenseBoundary } from './QuerySuspenseBoundary';
+import { useSuspenseQuery } from '@tanstack/react-query';
+import { getApplications, type Application } from '@/lib/application.service';
+import { isEmpty } from 'remeda';
 
 const StyledPaper = styled(PaperWithTopColor)(({ theme }) => ({
   marginTop: theme.spacing(2.5),
 }));
+
+function ApplicationContainer({ application }: { application: Application }) {
+  const { t } = useTranslations();
+
+  return (
+    <StyledPaper>
+      <OphTypography variant="h3">{application.hakuOid}</OphTypography>
+      <Typography>{t('hakemukset.hakutoiveet')}</Typography>
+      {application.hakukohteet.map((hk) => (
+        <Typography variant="h4" key={hk}>
+          {hk}
+        </Typography>
+      ))}
+    </StyledPaper>
+  );
+}
+
+function ApplicationList() {
+  const { t } = useTranslations();
+
+  const { data: applications } = useSuspenseQuery({
+    queryKey: ['applications'],
+    queryFn: getApplications,
+  });
+
+  return isEmpty(applications) ? (
+    <OphTypography>{t('hakemukset.ei-hakemuksia')}</OphTypography>
+  ) : (
+    <>
+      {applications?.map((application) => (
+        <ApplicationContainer
+          key={application.hakuOid}
+          application={application}
+        />
+      ))}
+    </>
+  );
+}
 
 export default function Applications() {
   const { t } = useTranslations();
@@ -16,7 +58,9 @@ export default function Applications() {
       <OphTypography variant="h2">
         {t('hakemukset.ajankohtaiset')}
       </OphTypography>
-      <StyledPaper>{t('hakemukset.ei-hakemuksia')}</StyledPaper>
+      <QuerySuspenseBoundary>
+        <ApplicationList />
+      </QuerySuspenseBoundary>
     </Box>
   );
 }
