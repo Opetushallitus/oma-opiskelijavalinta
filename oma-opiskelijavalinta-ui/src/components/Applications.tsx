6c96289a68db82699d82e308af64f69c7103c1cf
Risto Salama <risto.salama@gofore.com>
2025-11-07 13:06:44 +0200

    hakemusten tyylittelyä

--- oma-opiskelijavalinta-ui/src/components/Applications.tsx
+++ oma-opiskelijavalinta-ui/src/components/Applications.tsx
@@ -1,16 +1,64 @@
 import { Box, Typography } from '@mui/material';
-import { OphTypography } from '@opetushallitus/oph-design-system';
+import { ophColors, OphTypography } from '@opetushallitus/oph-design-system';
 import { PaperWithTopColor } from './PaperWithTopColor';
 import { styled } from '@/lib/theme';
 import { useTranslations } from '@/hooks/useTranslations';
 import { QuerySuspenseBoundary } from './QuerySuspenseBoundary';
 import { useSuspenseQuery } from '@tanstack/react-query';
-import { getApplications, type Application } from '@/lib/application.service';
+import {
+  getApplications,
+  type Application,
+  type Hakukohde,
+} from '@/lib/application.service';
 import { isEmpty } from 'remeda';
 
 const StyledPaper = styled(PaperWithTopColor)(({ theme }) => ({
   marginTop: theme.spacing(2.5),
+  display: 'flex',
+  flexDirection: 'column',
+  rowGap: theme.spacing(1),
 }));
+
+const HakukohdeContainer = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  columnGap: theme.spacing(1),
+  borderTop: '1px solid',
+  paddingTop: theme.spacing(2),
+  borderColor: ophColors.grey100,
+}));
+
+const OrderNumberBox = styled(Box)(({ theme }) => ({
+  color: ophColors.white,
+  backgroundColor: ophColors.grey400,
+  borderRadius: '4px',
+  fontWeight: 'bold',
+  fontSize: '1.3rem',
+  padding: `${theme.spacing(0.5)} ${theme.spacing(1)}`,
+}));
+
+function HakukohteetContainer({
+  hakukohteet,
+}: {
+  hakukohteet: Array<Hakukohde>;
+}) {
+  const { translateEntity } = useTranslations();
+
+  return (
+    <Box>
+      {hakukohteet.map((hk, idx) => (
+        <HakukohdeContainer key={hk.oid}>
+          <OrderNumberBox>{idx + 1}</OrderNumberBox>
+          <Box>
+            <Typography variant="h5">
+              {translateEntity(hk.jarjestyspaikkaHierarkiaNimi)}
+            </Typography>
+            <Typography variant="body1">{translateEntity(hk.nimi)}</Typography>
+          </Box>
+        </HakukohdeContainer>
+      ))}
+    </Box>
+  );
+}
 
 function ApplicationContainer({ application }: { application: Application }) {
   const { t, translateEntity } = useTranslations();
@@ -20,15 +68,10 @@ function ApplicationContainer({ application }: { application: Application }) {
       <OphTypography variant="h3">
         {translateEntity(application.haku.nimi)}
       </OphTypography>
-      <Typography>{t('hakemukset.hakutoiveet')}</Typography>
-      {application.hakukohteet.map((hk) => (
-        <Box key={hk.oid}>
-          <Typography variant="h4">
-            {translateEntity(hk.jarjestyspaikkaHierarkiaNimi)}
-          </Typography>
-          <Typography variant="body1">{translateEntity(hk.nimi)}</Typography>
-        </Box>
-      ))}
+      <Typography variant="h4" sx={{ fontWeight: 'normal' }}>
+        {t('hakemukset.hakutoiveet')}
+      </Typography>
+      <HakukohteetContainer hakukohteet={application.hakukohteet} />
     </StyledPaper>
   );
 }
