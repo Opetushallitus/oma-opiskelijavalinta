112bcf4efe5180e8d95744466a1012b832bce501
Risto Salama <risto.salama@gofore.com>
2025-11-14 08:55:50 +0200

    kälin testiympäristön määrittely .env tiedostosta

--- oma-opiskelijavalinta-ui/src/configuration.ts
+++ oma-opiskelijavalinta-ui/src/configuration.ts
@@ -8,15 +8,14 @@ export const localTranslations =
   import.meta.env.VITE_LOCAL_TRANSLATIONS === 'true';
 
 export const getConfiguration = async () => {
-  const backendDomainUrl = isDev ? 'https://localhost:8555' : '';
-
-  const sessionApiUrl = '/oma-opiskelijavalinta/api/session';
-
   let HAKIJA_DOMAIN = '';
   let VIRKAILIJA_DOMAIN = '';
   if (isDev) {
-    VIRKAILIJA_DOMAIN = 'https://virkailija.untuvaopintopolku.fi';
-    HAKIJA_DOMAIN = 'https://untuvaopintopolku.fi';
+    VIRKAILIJA_DOMAIN =
+      import.meta.env.VITE_VIRKAILIJA_DOMAIN ??
+      'https://virkailija.testiopintopolku.fi';
+    HAKIJA_DOMAIN =
+      import.meta.env.VITE_HAKIJA_DOMAIN ?? 'https://testiopintopolku.fi';
   } else if (typeof window !== 'undefined') {
     const host = window.location.hostname;
     VIRKAILIJA_DOMAIN = `https://virkailija.${host}`;
@@ -26,8 +25,8 @@ export const getConfiguration = async () => {
   return {
     routes: {
       yleiset: {
-        loginApiUrl: `${backendDomainUrl}/oma-opiskelijavalinta/api/login`,
-        sessionApiUrl: sessionApiUrl,
+        loginApiUrl: `/oma-opiskelijavalinta/api/login`,
+        sessionApiUrl: '/oma-opiskelijavalinta/api/session',
         lokalisointiUrl: `${VIRKAILIJA_DOMAIN}/lokalisointi/tolgee`,
         userUrl: `/oma-opiskelijavalinta/api/user`,
       },
