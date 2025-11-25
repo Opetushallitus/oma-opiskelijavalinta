b76a95d52eca72dea31b028c9e32f21620762cf4
Risto Salama <risto.salama@gofore.com>
2025-11-25 11:08:47 +0200

    Remove playwright docker script

--- oma-opiskelijavalinta-ui/scripts/run-playwright-tests-in-docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-PLAYWRIGHT=$(pnpm list -D "*playwright*test*" | tail -1)
-PLAYWRIGHT_VERSION=$(echo "${PLAYWRIGHT##* }")
-echo ${PLAYWRIGHT_VERSION}
-$PNPM_HOME="/pnpm"
-$PATH="$PNPM_HOME:$PATH"
-docker run --mount type=bind,source=$PWD,target=/app --user "$(id -u):$(id -g)" -w /app --ipc=host --net=host mcr.microsoft.com/playwright:v$PLAYWRIGHT_VERSION npx playwright test --ignore-engines $@
