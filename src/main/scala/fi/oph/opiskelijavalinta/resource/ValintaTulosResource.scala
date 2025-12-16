6bc1b5c9cf5c688e5fb83f520f73a17efabc2211
Risto Salama <risto.salama@gofore.com>
2025-12-16 15:48:17 +0200

    API valintatuloksien hakemiselle

---
+++ src/main/scala/fi/oph/opiskelijavalinta/resource/ValintaTulosResource.scala
@@ -0,0 +1,22 @@
+package fi.oph.opiskelijavalinta.resource
+
+import fi.oph.opiskelijavalinta.model.HakemuksenTulos
+import fi.oph.opiskelijavalinta.resource.ApiConstants.VALINTATULOS_PATH
+import fi.oph.opiskelijavalinta.service.VTSService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.{GetMapping, PathVariable, RequestMapping, RestController}
+
+@RequestMapping(path = Array(VALINTATULOS_PATH))
+@RestController
+class ValintaTulosResource @Autowired (vtsService: VTSService) {
+
+  @GetMapping(path = Array("/hakemus/{hakemusOid}/haku/{hakuOid}"))
+  def doVastaanotto(
+    @PathVariable hakemusOid: String,
+    @PathVariable hakuOid: String
+  ): ResponseEntity[HakemuksenTulos] = {
+    val result = vtsService.getValinnanTulokset(hakuOid, hakemusOid)
+    ResponseEntity.ok(result.get)
+  }
+}
