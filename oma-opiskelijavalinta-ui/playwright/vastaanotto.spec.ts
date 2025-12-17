4423fa00e5a6b30d7174c4d63778981acba2fcaa
Risto Salama <risto.salama@gofore.com>
2025-12-17 15:51:11 +0200

    Specs for vastaanotto actions

--- oma-opiskelijavalinta-ui/playwright/vastaanotto.spec.ts
+++ oma-opiskelijavalinta-ui/playwright/vastaanotto.spec.ts
@@ -5,6 +5,8 @@ import {
   mockAuthenticatedUser,
 } from './lib/playwrightUtils';
 import { hakemuksenTulosHyvaksytty, hakemus2 } from './mocks';
+import { clone } from 'remeda';
+import { VastaanottoTila } from '@/lib/valinta-tulos-types';
 
 test('Näyttää vastaanotettavan hakutoiveen', async ({ page }) => {
   await setup(page);
@@ -41,6 +43,143 @@ test('Näyttää virheen lähettäessä vastausta ilman vastaanoton valintaa', a
   ).toBeVisible();
 });
 
+test('Näyttää peruutettavan vahvistusdialogin lähetettäessä vastaanottoa', async ({
+  page,
+}) => {
+  await setup(page);
+  const vastaanotot = page.getByTestId('vastaanotot-hakemus-oid-2');
+  await vastaanotot
+    .getByRole('radio', { name: 'Otan tämän opiskelupaikan' })
+    .click();
+  const sendButton = vastaanotot.getByRole('button', {
+    name: 'Lähetä vastaus',
+  });
+  await sendButton.click();
+  await expect(
+    page.getByText('Vahvista opiskelupaikan vastaanotto'),
+  ).toBeVisible();
+  await page.getByRole('button', { name: 'Peruuta' }).click();
+  await expect(
+    page.getByText('Vahvista opiskelupaikan vastaanotto'),
+  ).toBeHidden();
+  await expect(
+    vastaanotot.getByRole('button', { name: 'Lähetä vastaus' }),
+  ).toBeVisible();
+});
+
+test('Lähettää vastaanoton onnistuneesti', async ({ page }) => {
+  await page.route(
+    '**/api/vastaanotto/hakemus/hakemus-oid-2/**',
+    async (route) => {
+      await route.fulfill({
+        status: 200,
+      });
+    },
+  );
+  await page.route(
+    '**/api/valintatulos/hakemus/hakemus-oid-2/**',
+    async (route) => {
+      await route.fulfill({
+        status: 200,
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify([
+          vastaanotettuTulos(VastaanottoTila.VASTAANOTTANUT_SITOVASTI),
+        ]),
+      });
+    },
+  );
+  await setup(page);
+  const vastaanotot = page.getByTestId('vastaanotot-hakemus-oid-2');
+  await vastaanotot
+    .getByRole('radio', { name: 'Otan tämän opiskelupaikan' })
+    .click();
+  const sendButton = vastaanotot.getByRole('button', {
+    name: 'Lähetä vastaus',
+  });
+  await sendButton.click();
+  await page
+    .getByRole('button', { name: 'Ota opiskelupaikka vastaan' })
+    .click();
+  await expect(
+    page.getByText('Opiskelupaikka vastaanotettu onnistuneesti'),
+  ).toBeVisible();
+  await expect(
+    vastaanotot.getByRole('button', { name: 'Lähetä vastaus' }),
+  ).toBeHidden();
+  await expect(
+    vastaanotot.getByText('Opiskelupaikka vastaanotettu', { exact: true }),
+  ).toBeVisible();
+});
+
+test('Lähettää vastaanoton epäonnistuneesti', async ({ page }) => {
+  await page.route(
+    '**/api/vastaanotto/hakemus/hakemus-oid-2/**',
+    async (route) => {
+      await route.fulfill({
+        status: 500,
+      });
+    },
+  );
+  await setup(page);
+  const vastaanotot = page.getByTestId('vastaanotot-hakemus-oid-2');
+  await vastaanotot
+    .getByRole('radio', { name: 'Otan tämän opiskelupaikan' })
+    .click();
+  const sendButton = vastaanotot.getByRole('button', {
+    name: 'Lähetä vastaus',
+  });
+  await sendButton.click();
+  await page
+    .getByRole('button', { name: 'Ota opiskelupaikka vastaan' })
+    .click();
+  await expect(page.getByText('Vastaanottotoiminto epäonnistui')).toBeVisible();
+  await expect(
+    vastaanotot.getByRole('button', { name: 'Lähetä vastaus' }),
+  ).toBeVisible();
+});
+
+test('Peruu vastaanoton onnistuneesti', async ({ page }) => {
+  await page.route(
+    '**/api/vastaanotto/hakemus/hakemus-oid-2/**',
+    async (route) => {
+      await route.fulfill({
+        status: 200,
+      });
+    },
+  );
+  await page.route(
+    '**/api/valintatulos/hakemus/hakemus-oid-2/**',
+    async (route) => {
+      await route.fulfill({
+        status: 200,
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify([vastaanotettuTulos(VastaanottoTila.PERUNUT)]),
+      });
+    },
+  );
+  await setup(page);
+  const vastaanotot = page.getByTestId('vastaanotot-hakemus-oid-2');
+  await vastaanotot
+    .getByRole('radio', { name: 'En ota tätä opiskelupaikkaa' })
+    .click();
+  const sendButton = vastaanotot.getByRole('button', {
+    name: 'Lähetä vastaus',
+  });
+  await sendButton.click();
+  await page
+    .getByRole('button', { name: 'En ota opiskelupaikkaa vastaan' })
+    .click();
+  await expect(
+    page.getByText('Opiskelupaikkaa ei vastaanotettu.'),
+  ).toBeVisible();
+  await expect(
+    vastaanotot.getByRole('button', { name: 'Lähetä vastaus' }),
+  ).toBeHidden();
+  await expect(
+    vastaanotot.getByText('Opiskelupaikkaa ei vastaanotettu', { exact: true }),
+  ).toBeVisible();
+});
+
 test('Vastaanotto on saavutettava', async ({ page }) => {
   await setup(page);
 
@@ -51,6 +190,13 @@ test('Vastaanotto on saavutettava', async ({ page }) => {
 
   await expectPageAccessibilityOk(page);
 });
+
+const vastaanotettuTulos = (vastaanottoTila: VastaanottoTila) => {
+  const tulos = clone(hakemuksenTulosHyvaksytty);
+  tulos.vastaanotettavuustila = 'EI_VASTAANOTETTAVISSA';
+  tulos.vastaanottotila = vastaanottoTila;
+  return tulos;
+};
 
 async function setup(page: Page) {
   const hyvaksyttyApplication = {
