d8760b7cf2819e03fec18c2c9b8ed1f52f55d7fe
Marja Kari <marja.kari@amban.fi>
2026-01-13 09:21:44 +0200

    OPHYOS-30 spotless fix

--- src/main/scala/fi/oph/opiskelijavalinta/model/HakemuksenTulos.scala
+++ src/main/scala/fi/oph/opiskelijavalinta/model/HakemuksenTulos.scala
@@ -8,27 +8,27 @@ case class HakemuksenTulos(
 )
 
 case class HakutoiveenTulos(
-                             hakukohdeOid: Option[String],
-                             hakukohdeNimi: Option[String],
-                             tarjoajaOid: Option[String],
-                             tarjoajaNimi: Option[String],
-                             valintatapajonoOid: Option[String],
-                             valintatila: Option[String],
-                             vastaanottotila: Option[String],
-                             ilmoittautumistila: Option[Ilmoittautumistila],
-                             vastaanotettavuustila: Option[String],
-                             vastaanottoDeadline: Option[String],
-                             viimeisinHakemuksenTilanMuutos: Option[String],
-                             hyvaksyttyJaJulkaistuDate: Option[String],
-                             julkaistavissa: Option[Boolean],
-                             ehdollisestiHyvaksyttavissa: Option[Boolean],
-                             ehdollisenHyvaksymisenEhtoKoodi: Option[String],
-                             ehdollisenHyvaksymisenEhtoFI: Option[String],
-                             ehdollisenHyvaksymisenEhtoSV: Option[String],
-                             ehdollisenHyvaksymisenEhtoEN: Option[String],
-                             tilanKuvaukset: Option[Map[String, String]],
-                             showMigriURL: Option[Boolean],
-                             jonokohtaisetTulostiedot: List[JonokohtainenTulostieto] // empty list if missing
+  hakukohdeOid: Option[String],
+  hakukohdeNimi: Option[String],
+  tarjoajaOid: Option[String],
+  tarjoajaNimi: Option[String],
+  valintatapajonoOid: Option[String],
+  valintatila: Option[String],
+  vastaanottotila: Option[String],
+  ilmoittautumistila: Option[Ilmoittautumistila],
+  vastaanotettavuustila: Option[String],
+  vastaanottoDeadline: Option[String],
+  viimeisinHakemuksenTilanMuutos: Option[String],
+  hyvaksyttyJaJulkaistuDate: Option[String],
+  julkaistavissa: Option[Boolean],
+  ehdollisestiHyvaksyttavissa: Option[Boolean],
+  ehdollisenHyvaksymisenEhtoKoodi: Option[String],
+  ehdollisenHyvaksymisenEhtoFI: Option[String],
+  ehdollisenHyvaksymisenEhtoSV: Option[String],
+  ehdollisenHyvaksymisenEhtoEN: Option[String],
+  tilanKuvaukset: Option[Map[String, String]],
+  showMigriURL: Option[Boolean],
+  jonokohtaisetTulostiedot: List[JonokohtainenTulostieto] // empty list if missing
 )
 
 case class Ilmoittautumistila(
