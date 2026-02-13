826057a0d8c1ee5eac7f47315eb1b3c9b663a75f
Risto Salama <risto.salama@gofore.com>
2026-02-13 10:26:24 +0200

    Remove past info

--- oma-opiskelijavalinta-ui/src/components/hakemukset/MennytHakemusContainer.tsx
+++ oma-opiskelijavalinta-ui/src/components/hakemukset/MennytHakemusContainer.tsx
@@ -1,21 +1,13 @@
 import { Box } from '@mui/material';
 import { OphTypography } from '@opetushallitus/oph-design-system';
-import { styled } from '@/lib/theme';
 import { useTranslations } from '@/hooks/useTranslations';
-import { isTruthy } from 'remeda';
 import { ExternalLink } from '../ExternalLink';
 import { Hakutoive } from '../hakukohde/Hakutoive';
 import { HakemusPaper } from './HakemusPaper';
 import type { Hakukohde } from '@/lib/kouta-types';
 import type { Hakemus } from '@/lib/hakemus-types';
 
-const StyledInfoBox = styled(Box)(({ theme }) => ({
-  display: 'flex',
-  flexDirection: 'row',
-  columnGap: theme.spacing(0.5),
-}));
-
-function PastHakukohteetContainer({
+function MenneetHakukohteetContainer({
   hakemus,
   hakukohteet,
 }: {
@@ -37,26 +29,6 @@ function PastHakukohteetContainer({
   );
 }
 
-function PastTilaInfo({ application }: { application: Hakemus }) {
-  const { t } = useTranslations();
-
-  return (
-    <StyledInfoBox>
-      {isTruthy(application.haku) && (
-        <OphTypography>
-          {t('hakemukset.tilankuvaukset.kaikki-julkaistu')}
-        </OphTypography>
-      )}
-      {isTruthy(application.modifyLink) && (
-        <ExternalLink
-          href={application.modifyLink ?? ''}
-          name={t('hakemukset.nayta')}
-        />
-      )}
-    </StyledInfoBox>
-  );
-}
-
 export function MennytHakemusContainer({ hakemus }: { hakemus: Hakemus }) {
   const { t, translateEntity } = useTranslations();
 
@@ -65,11 +37,14 @@ export function MennytHakemusContainer({ hakemus }: { hakemus: Hakemus }) {
       <OphTypography variant="h3">
         {translateEntity(hakemus?.haku?.nimi)}
       </OphTypography>
-      <PastTilaInfo application={hakemus} />
+      <ExternalLink
+        href={hakemus.modifyLink ?? ''}
+        name={t('hakemukset.nayta')}
+      />
       <OphTypography variant="h4" sx={{ fontWeight: 'normal' }}>
         {t('hakemukset.hakutoiveet')}
       </OphTypography>
-      <PastHakukohteetContainer
+      <MenneetHakukohteetContainer
         hakemus={hakemus}
         hakukohteet={hakemus?.hakukohteet ?? []}
       />
