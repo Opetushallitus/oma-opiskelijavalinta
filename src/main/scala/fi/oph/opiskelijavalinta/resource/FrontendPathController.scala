5bbd9bc0221c4ca49e34b0285a35b9347e022ea9
Marja Kari <marja.kari@amban.fi>
2026-02-19 13:27:43 +0200

    OPHYOS-78 forward unknown paths to index

---
+++ src/main/scala/fi/oph/opiskelijavalinta/resource/FrontendPathController.scala
@@ -0,0 +1,18 @@
+package fi.oph.opiskelijavalinta.resource
+
+import jakarta.servlet.http.HttpServletRequest
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class FrontendPathController {
+
+  @GetMapping("/**")
+  def forward(request: HttpServletRequest): String = {
+    val path = request.getRequestURI
+    if (path.startsWith("/api") || path.startsWith("/assets") || path.startsWith("/js") || path == "/index.html")
+      path // Let Spring handle normally 
+    else
+      "forward:/index.html"
+  }
+}
