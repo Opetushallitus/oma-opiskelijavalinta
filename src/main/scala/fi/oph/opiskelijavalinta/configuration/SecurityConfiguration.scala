531b352087c2a0121e0111a788cc1fd33289b36b
Marja Kari <marja.kari@amban.fi>
2026-02-20 10:46:48 +0200

    OPHYOS-78 logging

--- src/main/scala/fi/oph/opiskelijavalinta/configuration/SecurityConfiguration.scala
+++ src/main/scala/fi/oph/opiskelijavalinta/configuration/SecurityConfiguration.scala
@@ -111,22 +111,31 @@ class SecurityConfiguration {
 
   private def isFrontEndRoute: String => Boolean = path =>
     path.equals("/index.html") || path.equals("/") || path.startsWith("/token") || path.startsWith("/assets") || path
-      .startsWith("/js")
+      .startsWith("/js") || path.startsWith("/redirect")
 
   @Bean
   def frontendResourceFilter: Filter = (request: ServletRequest, response: ServletResponse, chain: FilterChain) => {
+    LOG.info(s"FrontendResourceFilter: ${request.getRemoteAddr} ${request.asInstanceOf[HttpServletRequest].getRequestURI}")
     val req         = request.asInstanceOf[HttpServletRequest]
     val res         = response.asInstanceOf[HttpServletResponse]
     val contextPath = req.getContextPath
     val path        = req.getRequestURI.stripPrefix(contextPath)
     val queryString = Option(req.getQueryString).map(q => s"?$q").getOrElse("")
     val isForwarded = request.getAttribute("custom.forwarded") != null
-    if (!isForwarded && isFrontEndRoute(path)) {
+    val fullPathWithQuery = contextPath + path + queryString
+    val strippedPathWithQuery = contextPath + path.stripSuffix("index.html") + queryString.replaceAll("[?&]continue", "")
+    // Karsitaan URL:sta pois tarpeettomat osat ennen forwardia
+    if (!isForwarded && isFrontEndRoute(path) && !fullPathWithQuery.equals(strippedPathWithQuery)) {
+      LOG.info(s"Redirecting to stripped path: $strippedPathWithQuery")
+      res.sendRedirect(strippedPathWithQuery)
+    } else if (!isForwarded && isFrontEndRoute(path)) {
+      LOG.info(s"Forwarding to index.html")
       // Lisätään attribuutti, jotta voidaan välttää redirect-looppi edellisessä haarassa
       request.setAttribute("custom.forwarded", true)
       // Forwardoidaan frontend-pyyntö html-tiedostoon
       request.getRequestDispatcher("/index.html").forward(request, response)
     } else {
+      LOG.info(s"continue filter chain for path: $path")
       chain.doFilter(request, response)
     }
   }
