4b55e84de90350e6908bf6de48d9cfb9ef886ff9
Risto Salama <risto.salama@gofore.com>
2026-02-26 07:15:31 +0200

    Näytä virhesivu jos virhe tulee user-rajapinnasta

--- oma-opiskelijavalinta-ui/playwright/virhesivu.spec.ts
+++ oma-opiskelijavalinta-ui/playwright/virhesivu.spec.ts
@@ -7,19 +7,12 @@ import {
 
 test('Näyttää virhesivun', async ({ page }) => {
   await setup(page);
-  await expect(page).toHaveTitle('Oma Opiskelijavalinta');
-  await expect(
-    page.getByRole('heading', { name: 'Nyt meni jokin pieleen' }),
-  ).toBeVisible();
-  await expect(page.getByText('Tapahtui palvelinvirhe. Lataa')).toBeVisible();
-  await expect(
-    page.getByRole('button', { name: 'Lataa sivu uudestaan' }),
-  ).toBeVisible();
+  await assertVirheSivu(page);
 });
 
 test('Lataa sivun uudelleen onnistuneesti', async ({ page }) => {
   await setup(page);
-  await expect(page.getByText('Tapahtui palvelinvirhe. Lataa')).toBeVisible();
+  await assertVirheSivu(page);
   await page.unroute('**/api/session');
   await page.route('**/api/session', async (route) => {
     await route.fulfill({
@@ -34,20 +27,47 @@ test('Lataa sivun uudelleen onnistuneesti', async ({ page }) => {
   await expect(page.getByText('Ruhtinas Nukettaja')).toBeVisible();
 });
 
+test('Näyttää virhesivun kun virhe tulee user-rajapinnasta', async ({
+  page,
+}) => {
+  await setup(page, '**/api/user');
+  await assertVirheSivu(page);
+});
+
+test('Ei näytä virhesivua kun virhe tulee hakemukset-rajapinnasta', async ({
+  page,
+}) => {
+  await setup(page, '**/api/hakemukset');
+  await expect(page.getByText('Tapahtui palvelinvirhe. Lataa')).toBeHidden();
+  await expect(
+    page.getByRole('heading', { name: 'Nyt meni jokin pieleen' }),
+  ).toBeHidden();
+  await expect(page.getByText('Palvelinvirhe')).toHaveCount(2);
+});
+
 test('Virhesivun saavutettavuus', async ({ page }) => {
   await setup(page);
   await page.goto('');
-  await expect(
-    page.getByRole('heading', { name: 'Nyt meni jokin pieleen' }),
-  ).toBeVisible();
+  await assertVirheSivu(page);
   await expectPageAccessibilityOk(page);
 });
 
-async function setup(page: Page) {
-  await page.route('**/api/session', async (route) => {
+async function setup(page: Page, api: string = '**/api/session') {
+  await page.route(api, async (route) => {
     await route.fulfill({
       status: 500,
     });
   });
   await page.goto('');
+}
+
+async function assertVirheSivu(page: Page) {
+  await expect(page).toHaveTitle('Oma Opiskelijavalinta');
+  await expect(
+    page.getByRole('heading', { name: 'Nyt meni jokin pieleen' }),
+  ).toBeVisible();
+  await expect(page.getByText('Tapahtui palvelinvirhe. Lataa')).toBeVisible();
+  await expect(
+    page.getByRole('button', { name: 'Lataa sivu uudestaan' }),
+  ).toBeVisible();
 }
