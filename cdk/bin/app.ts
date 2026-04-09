0f3aa4ba75ce617803348251ddabdb802f56cb89
Marja Kari <marja.kari@amban.fi>
2026-04-09 13:18:09 +0300

    OPHYOS-71 destroy loadtest stack workflow

--- cdk/bin/app.ts
+++ cdk/bin/app.ts
@@ -6,7 +6,7 @@ const app = new cdk.App();
 
 const environmentName = app.node.tryGetContext('environment') || 'pallero';
 
-new LoadtestStack(app, 'LoadtestStack', {
+new LoadtestStack(app, 'oma-opiskelijavalinta-LoadtestStack', {
   env: { region: 'eu-west-1' },
   environmentName,
 });
