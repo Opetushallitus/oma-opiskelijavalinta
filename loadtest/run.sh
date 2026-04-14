eface389491fb98912bc0c9ef972f1baf168ff56
Marja Kari <marja.kari@amban.fi>
2026-04-14 12:05:14 +0300

    OPHYOS-71 päivitetty run-skripti

--- loadtest/run.sh
+++ loadtest/run.sh
@@ -3,27 +3,25 @@ set -e
 
 ENVIRONMENT=$1
 S3_BUCKET=$2
+TEST_TYPE=${3:-load}   # default = load test if not provided
 
 cd /home/ssm-user/loadtest
 
-# Select environment-specific user file
-USER_FILE="users-${ENVIRONMENT}.json"
-if [ ! -f "$USER_FILE" ]; then
-  echo "User file $USER_FILE not found!"
-  exit 1
-fi
-
-# Timestamp for results
 TIMESTAMP=$(date +%Y%m%d-%H%M%S)
-RESULT_FILE="results-${ENVIRONMENT}-${TIMESTAMP}.json"
+RESULT_FILE="results-${ENVIRONMENT}-${TEST_TYPE}-${TIMESTAMP}.json"
 
-# Run k6
-echo "Running k6 load test for environment $ENVIRONMENT..."
+echo "Running k6 load test..."
+echo "ENVIRONMENT=$ENVIRONMENT"
+echo "TEST_TYPE=$TEST_TYPE"
+
 pnpm install
-k6 run script.js --out json=${RESULT_FILE}
 
-# Upload results to S3
+k6 run script.js \
+  -e ENVIRONMENT=${ENVIRONMENT} \
+  -e TEST_TYPE=${TEST_TYPE} \
+  --out json=${RESULT_FILE}
+
 echo "Uploading results to s3://${S3_BUCKET}/${RESULT_FILE}"
 aws s3 cp ${RESULT_FILE} s3://${S3_BUCKET}/${RESULT_FILE}
 
-echo "Load test finished. Results uploaded to S3."
+echo "Load test finished."
