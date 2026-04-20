1dba06d25e03317c173b13844193bdb48934b9b5
Marja Kari <marja.kari@amban.fi>
2026-04-20 15:54:34 +0300

    spotless fix

--- src/main/scala/fi/oph/opiskelijavalinta/Constants.scala
+++ src/main/scala/fi/oph/opiskelijavalinta/Constants.scala
@@ -2,7 +2,7 @@ package fi.oph.opiskelijavalinta
 
 object Constants {
 
-  final val CALLER_ID                 = "1.2.246.562.10.00000000001.oma-opiskelijavalinta"
-  final val OPH_ORGANISAATIO_OID      = "1.2.246.562.10.00000000001"
-  final val KOUTA_HAKU_OID_LENGTH     = 35
+  final val CALLER_ID             = "1.2.246.562.10.00000000001.oma-opiskelijavalinta"
+  final val OPH_ORGANISAATIO_OID  = "1.2.246.562.10.00000000001"
+  final val KOUTA_HAKU_OID_LENGTH = 35
 }
