6008d122ff8537b0efbffd8003584c1ea0868e55
Risto Salama <risto.salama@gofore.com>
2026-05-11 14:12:48 +0300

    Päätettävien oikeuksien näyttäminen WIP

---
+++ oma-opiskelijavalinta-ui/src/components/vastaanotto/PaatettavatOikeudetInfo.tsx
@@ -0,0 +1,9 @@
+import type { PaatettavaOpiskeluOikeus } from "@/lib/valinta-tulos-types";
+
+export function PaatettavatOikeudetInfo(
+  {oikeudet}: {oikeudet: Array<PaatettavaOpiskeluOikeus>}) {
+
+    return (
+      
+    )
+}
