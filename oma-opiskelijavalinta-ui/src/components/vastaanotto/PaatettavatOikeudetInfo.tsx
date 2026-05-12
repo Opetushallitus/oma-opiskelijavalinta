f4c8752b14d907e777ddd05d70585cacc493063d
Risto Salama <risto.salama@gofore.com>
2026-05-12 08:26:38 +0300

    Päättyvät oikeudet info vastaanottoon

--- oma-opiskelijavalinta-ui/src/components/vastaanotto/PaatettavatOikeudetInfo.tsx
+++ oma-opiskelijavalinta-ui/src/components/vastaanotto/PaatettavatOikeudetInfo.tsx
@@ -1,9 +1,31 @@
-import type { PaatettavaOpiskeluOikeus } from "@/lib/valinta-tulos-types";
+import { OphTypography } from '@opetushallitus/oph-design-system';
+import type { PaatettavaOpiskeluOikeus } from '@/lib/valinta-tulos-types';
+import { useTranslations } from '@/hooks/useTranslations';
+import { BulletedList, BulletItem } from '../BulletedList';
 
-export function PaatettavatOikeudetInfo(
-  {oikeudet}: {oikeudet: Array<PaatettavaOpiskeluOikeus>}) {
+export function PaatettavatOikeudetInfo({
+  oikeudet,
+}: {
+  oikeudet: Array<PaatettavaOpiskeluOikeus>;
+}) {
+  const { t, translateEntity } = useTranslations();
 
-    return (
-      
-    )
+  return (
+    <>
+      <OphTypography variant="h5">{t('tulos.yos.otsikko')}</OphTypography>
+      <OphTypography>{t('tulos.yos.kuvaus')}</OphTypography>
+      <BulletedList>
+        {oikeudet.map((oikeus) => (
+          <BulletItem
+            key={`paatettava-oikeus-${oikeus.organisaatioOid ?? oikeus.tunniste}`}
+          >
+            <OphTypography>
+              {translateEntity(oikeus.organisaatioNimi)}
+            </OphTypography>
+            <OphTypography>{translateEntity(oikeus.nimi)}</OphTypography>
+          </BulletItem>
+        ))}
+      </BulletedList>
+    </>
+  );
 }
