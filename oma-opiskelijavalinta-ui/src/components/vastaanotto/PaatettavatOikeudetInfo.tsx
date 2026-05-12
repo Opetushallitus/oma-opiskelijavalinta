1d1d02b94f33b6b136db7fa4b17e35f2516e9fc2
Risto Salama <risto.salama@gofore.com>
2026-05-12 09:50:35 +0300

    Alustava linkki yos ohjeisiin

--- oma-opiskelijavalinta-ui/src/components/vastaanotto/PaatettavatOikeudetInfo.tsx
+++ oma-opiskelijavalinta-ui/src/components/vastaanotto/PaatettavatOikeudetInfo.tsx
@@ -2,6 +2,8 @@ import { OphTypography } from '@opetushallitus/oph-design-system';
 import type { PaatettavaOpiskeluOikeus } from '@/lib/valinta-tulos-types';
 import { useTranslations } from '@/hooks/useTranslations';
 import { BulletedList, BulletItem } from '../BulletedList';
+import { ExternalLink } from '../ExternalLink';
+import { useConfig } from '@/configuration';
 
 export function PaatettavatOikeudetInfo({
   oikeudet,
@@ -9,6 +11,10 @@ export function PaatettavatOikeudetInfo({
   oikeudet: Array<PaatettavaOpiskeluOikeus>;
 }) {
   const { t, translateEntity } = useTranslations();
+
+  const config = useConfig();
+
+  const yosHref = `${config.routes.yleiset.konfo}`;
 
   return (
     <>
@@ -25,6 +31,7 @@ export function PaatettavatOikeudetInfo({
             <OphTypography>{translateEntity(oikeus.nimi)}</OphTypography>
           </BulletItem>
         ))}
+        <ExternalLink href={yosHref} name={t('tulos.yos.linkki')} />
       </BulletedList>
     </>
   );
