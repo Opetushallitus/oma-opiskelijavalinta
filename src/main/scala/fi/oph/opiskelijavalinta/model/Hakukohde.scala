763c03a538a72598376ba7147512a53a12f353df
Risto Salama <risto.salama@gofore.com>
2026-06-15 15:12:37 +0300

    Review changes

--- src/main/scala/fi/oph/opiskelijavalinta/model/Hakukohde.scala
+++ src/main/scala/fi/oph/opiskelijavalinta/model/Hakukohde.scala
@@ -4,7 +4,7 @@ case class YhdenPaikanSaanto(voimassa: Boolean)
 
 case class PaateltyAlkamiskausi(kausiUri: Option[String])
 
-case class PaateltyAlkamisAjankohta(pvm: String, henkilokohtainenSuunnitelma: Boolean)
+case class PaateltyAlkamisajankohta(pvm: String, henkilokohtainenSuunnitelma: Boolean)
 
 case class Hakukohde(
   oid: String,
@@ -12,7 +12,7 @@ case class Hakukohde(
   jarjestyspaikkaHierarkiaNimi: TranslatedName,
   uudenOpiskelijanUrl: Option[TranslatedName],
   paateltyAlkamiskausi: Option[PaateltyAlkamiskausi],
-  paateltyAlkamisajankohta: Option[PaateltyAlkamisAjankohta],
+  paateltyAlkamisajankohta: Option[PaateltyAlkamisajankohta],
   yhdenPaikanSaanto: YhdenPaikanSaanto = YhdenPaikanSaanto(false)
 )
 
