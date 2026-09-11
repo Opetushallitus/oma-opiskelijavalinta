b1f6f90b922468a6d901e5269198c1090c7acf0a
Tomi Katajanheimo <tomi@katajanheimo.com>
2026-09-11 10:43:01 +0300

    OY-5804 playwright-testi: etusivu näyttää oikein kun käyttäjällä ei ole oppijanumeroa

--- oma-opiskelijavalinta-ui/playwright/etusivu.spec.ts
+++ oma-opiskelijavalinta-ui/playwright/etusivu.spec.ts
@@ -2,6 +2,7 @@ import { expect, test, type Page } from '@playwright/test';
 import {
   expectPageAccessibilityOk,
   mockAuthenticatedUser,
+  mockAuthenticatedUserWithoutOppijanumero,
   mockHakemuksetFetch,
 } from './lib/playwrightUtils';
 
@@ -34,6 +35,20 @@ test('Etusivun saavutettavuus', async ({ page }) => {
     page.getByText('Hakemukset ja opiskelupaikan vastaanotto'),
   ).toBeVisible();
   await expectPageAccessibilityOk(page);
+});
+
+test('Näyttää etusivun kun käyttäjällä ei ole oppijanumeroa', async ({
+  page,
+}) => {
+  await mockAuthenticatedUserWithoutOppijanumero(page);
+  await mockHakemuksetFetch(page, { current: [], old: [] });
+  await page.goto('');
+  await expect(
+    page.getByText('Sinulla ei ole ajankohtaisia opiskelupaikan hakemuksia.'),
+  ).toBeVisible();
+  await expect(
+    page.getByText('Sinulla ei ole aiempia hakemuksia'),
+  ).toBeVisible();
 });
 
 async function setup(page: Page) {
